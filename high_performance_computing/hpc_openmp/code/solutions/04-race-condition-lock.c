8edc325c339356bffaaf71f095bc14b5ee91e20a
Mehtap O. Arabaci <m.ozbey-arabaci@soton.ac.uk>
2024-12-02 12:59:16 +0000

    Episode 4 code updates: corrected indentation in race condition lock solution

--- high_performance_computing/hpc_openmp/code/solutions/04-race-condition-lock.c
+++ high_performance_computing/hpc_openmp/code/solutions/04-race-condition-lock.c
@@ -5,29 +5,31 @@
 #define ARRAY_SIZE 524288
 
 int main(int argc, char **argv) {
-  float sum = 0;
-  float array[ARRAY_SIZE];
+    float sum = 0;
+    float array[ARRAY_SIZE];
 
-  omp_set_num_threads(4);
+    omp_set_num_threads(4);
 
-#pragma omp parallel for schedule(static)
-  for (int i = 0; i < ARRAY_SIZE; ++i) {
-    array[i] = cos(M_PI * i);
-  }
+    #pragma omp parallel for schedule(static)
+    for (int i = 0; i < ARRAY_SIZE; ++i) {
+        array[i] = cos(M_PI * i);
+    }
 
-  omp_lock_t lock;
-  omp_init_lock(&lock);
+    omp_lock_t lock;
+    omp_init_lock(&lock);
 
-#pragma omp parallel for schedule(static)
-  for (int i = 0; i < ARRAY_SIZE; i++) {
-    omp_set_lock(&lock);
-    sum += array[i];
-    omp_unset_lock(&lock);
-  }
 
-  printf("Sum: %f\n", sum);
+    #pragma omp parallel for schedule(static)
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        omp_set_lock(&lock);
+        sum += array[i];
+        omp_unset_lock(&lock);
+    }
 
-  omp_destroy_lock(&lock);
 
-  return 0;
+    printf("Sum: %f\n", sum);
+
+    omp_destroy_lock(&lock);
+
+    return 0;
 }
